4c6fd6c99142ceb6765db64fcbe86dc57f9dad16
김성경 <gimseong-gyeong@gimseong-gyeong-ui-MacBook-Pro.local>
2018-07-16 07:05:25 +0900

    20180716

--- chat1/chat_jsp.js
+++ chat1/chat_jsp.js
@@ -10,18 +10,36 @@ server.listen(3000);
 // });
 
 var io = require('socket.io')(server);
-io.on('connect',function(socket) {
-  console.log('클라이언트 접속');
 
-  socket.on('disconnect',function(){
+
+var count =1;
+io.on('connect',function(socket) {
+  console.log('클라이언트 접속 : ' + socket.id);
+  var name = 'user' + count++;
+
+  io.to(socket.id).emit('change name', name);
+
+
+
+  socket.on('disconnect',function(req,res){
     console.log('클라이언트 접속 종료');
+
+    io.emit('disconnect',req);
+
   });
 
   socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
+    console.log(name + ':'  + msg);
+
   });
 
   // setInterval(function(){
   //   socket.emit('message','메시지');
   // }, 3000);
+});
+
+io.on('connection', function(socket){
+socket.on('chat message', function(msg,name){
+  io.emit('chat message', msg,name);
+});
 });
